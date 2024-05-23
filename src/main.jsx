632f9070785e8a34ac4e34448159fbc6a67d893b
John MacKenzie <john.mackenzie@icf.com>
2024-05-23 11:50:23 -0400

    basic layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const skip = document.querySelector('[href="#main"]');
+const main = document.getElementById("main");
+if (skip && main) {
+  skip.addEventListener("click", (event) => {
+    event.preventDefault();
+    main.setAttribute("tabindex", "-1");
+    main.focus();
+    main.removeAttribute("tabindex");
+  });
+}
+
+ReactDOM.createRoot(document.getElementById("main")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
